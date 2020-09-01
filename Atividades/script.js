28368db93e5d9e9e829755a178a15a78c3c9710a
fabricio <fabricioig863@gmail.com>
2020-09-01 10:53:09 -0300

    Resolução das atividades do curso

---
+++ Atividades/script.js
@@ -0,0 +1,9 @@
+// 2) 
+// function pares_no_intervalo(num1, num2){
+//     for(let i = num1; i <= num2; i++){
+//         if((i % 2) == 0){
+//             console.log(`É par:${i}`)
+//         }
+//     }
+// }
+// pares_no_intervalo(1,42)
